882806a7c9eb143572a89549dd0e29b60833761e
AmzylMohcine <amzylmohcine@gmail.com>
2023-04-29 12:36:34 +0200

    add button active

--- src/Components/Filtre.tsx
+++ src/Components/Filtre.tsx
@@ -17,9 +17,17 @@ const Filtre = ({ setActiveGenre, activeGenre, setFiltred, movies }) => {
 
   return (
     <motion.div layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} className="filtre-container">
-      <button onClick={() => setActiveGenre(0)}> All </button>
-      <button onClick={() => setActiveGenre(35)}> Comedy</button>
-      <button onClick={() => setActiveGenre(28)}>Action</button>
+      <button className={activeGenre === 0 ? "active" : " "} onClick={() => setActiveGenre(0)}>
+        {" "}
+        All{" "}
+      </button>
+      <button className={activeGenre === 35 ? "active" : " "} onClick={() => setActiveGenre(35)}>
+        {" "}
+        Comedy
+      </button>
+      <button className={activeGenre === 28 ? "active" : " "} onClick={() => setActiveGenre(28)}>
+        Action
+      </button>
     </motion.div>
   )
 }
